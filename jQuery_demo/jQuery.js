5d0e7b29f00135bddd3b6c473efaba51199204ef
2lifetop <jiawen14th@gmail.com>
2021-01-04 15:40:17 +0800

    ver 0.1.3

--- jQuery_demo/jQuery.js
+++ jQuery_demo/jQuery.js
@@ -1,13 +1,29 @@
-window.jQuery = function(selector){
-    const elements = document.querySelectorAll(selector)
+window.jQuery = function(selectorOrArray){
+    let elements
+    if (typeof selectorOrArray === 'string'){
+         elements = document.querySelectorAll(selectorOrArray)
+    }else if (selectorOrArray instanceof Array){
+         elements = selectorOrArray
+    }
+    
     //api 可以操作elements
     return {
+        find(selector){
+            let array = []
+            for(let i=0;i<elements.length;i++){
+                const elements2 = Array.from(elements[i].querySelectorAll(selector))
+                array = array.concat(elements2) 
+            }
+            const newApi = jQuery(array)
+            return newApi
+        },
         //闭包：函数访问外部变量
         addClass(className){
             for(let i=0;i<elements.length;i++){
                 elements[i].classList.add(className)
             }
             return this
-        }
+        },
+        
     }  
 }
--- jQuery_demo/main.js
+++ jQuery_demo/main.js
@@ -1,3 +1,4 @@
-jQuery('.test')//不返回元素而是返回api对象
-    .addClass('red')
-    .addClass('bule')//遍历刚才所选择的元素，添加 .red 之后返回的依旧是api对象所以可以继续操作，链式操作
+const x1 = jQuery('.test1').find('.child')
+console.log(x1)
+x1.addClass('red')
+    .addClass('GREEN')
