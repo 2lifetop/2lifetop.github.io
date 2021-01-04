a30c7b4a86be924b2822b75a5e98489e7fecbdf3
2lifetop <jiawen14th@gmail.com>
2021-01-04 14:29:43 +0800

    ver 0.1.1

---
+++ jQuery_demo/jQuery.js
@@ -0,0 +1,13 @@
+window.jQuery = function(selector){
+    const elements = document.querySelectorAll(selector)
+    //api 可以操作elements
+    return {
+        //闭包：函数访问外部变量
+        addClass(className){
+            for(let i=0;i<elements.length;i++){
+                elements[i].classList.add(className)
+            }
+            return this
+        }
+    }  
+}
---
+++ jQuery_demo/main.js
@@ -0,0 +1,2 @@
+const api = window.jQuery('.test')//不返回元素而是返回api对象
+api.addClass('red').addClass('bule')//遍历刚才所选择的元素，添加 .red 之后返回的依旧是api对象所以可以继续操作，链式操作
